014f42c8a8c717f48ebd798af02c52fde76337dc
Ross Perry <perryr16@gmail.com>
2021-04-02 16:00:46 -0600

    react-redux

---
+++ z_sandbox/sandbox2.js
@@ -0,0 +1,9 @@
+const fxn1 = () => {
+   return 'dog'
+}
+
+console.log(fxn1)
+console.log(fxn1())
+
+// to reference a funciton (that will be called) you dont need the '()'
+// to actually call the function, you need the '()'
